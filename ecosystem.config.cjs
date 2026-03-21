26073c77ecd6acf81282288f751d4df1367543df
m <m@emunoMacBook-Air.local>
2026-03-21 18:07:21 +0900

    feat: monkey複数インスタンス対応 (pm2管理)

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,28 @@
 module.exports = {
-  apps: [{
-    name: 'llamune_monkey',
-    script: 'npm',
-    args: 'run dev',
-    cwd: '/Users/m/dev/llamune_monkey',
-    out_file: '/Users/m/dev/llamune_monkey/logs/monkey.log',
-    error_file: '/Users/m/dev/llamune_monkey/logs/monkey-error.log',
-    merge_logs: true,
-  }]
+  apps: [
+    {
+      name: 'monkey-1',
+      script: 'npm',
+      args: 'run dev',
+      cwd: '/Users/m/dev/llamune_monkey',
+      out_file: '/Users/m/dev/llamune_monkey/logs/monkey-1.log',
+      error_file: '/Users/m/dev/llamune_monkey/logs/monkey-1.log',
+      merge_logs: true,
+      env: {
+        PORT: '4000',
+      },
+    },
+    {
+      name: 'monkey-2',
+      script: 'npm',
+      args: 'run dev',
+      cwd: '/Users/m/dev/llamune_monkey',
+      out_file: '/Users/m/dev/llamune_monkey/logs/monkey-2.log',
+      error_file: '/Users/m/dev/llamune_monkey/logs/monkey-2.log',
+      merge_logs: true,
+      env: {
+        PORT: '4001',
+      },
+    },
+  ]
 }
